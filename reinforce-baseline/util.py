28de0460e2abafac6b23f21d9c4ba85cdda461a6
yanmingzhu <yanmingzhu06@gmail.com>
2024-12-02 02:18:44 -0500

    clean up

--- reinforce-baseline/util.py
+++ reinforce-baseline/util.py
@@ -18,17 +18,14 @@ def train_agent(seed = 89, max_steps = 1000000):
 
     torch.manual_seed(seed)
 
-    # Reinitialize agent every seed
     agent = Agent(obs_space_dims, action_space_dims)
     print(f"discount factor = {agent.gamma}")
     rewards_over_episodes = []
     rewards_to_plot = []
 
-    #for episode in range(max_episodes):
     trainingSteps = 0
     episode = 0
     while trainingSteps < max_steps:
-        # gymnasium v26 requires users to set seed while resetting the environment
         obs, info = wrapped_env.reset(seed=seed)
 
         done = False
@@ -36,16 +33,9 @@ def train_agent(seed = 89, max_steps = 1000000):
         while not done:
             action = agent.sample_action(obs)
 
-            # Step return type - `tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]`
-            # These represent the next observation, the reward from the step,
-            # if the episode is terminated, if the episode is truncated and
-            # additional info from the step
             obs, reward, terminated, truncated, info = wrapped_env.step(action)
             agent.rewards.append(reward)
 
-            # End the episode when either truncated or terminated is true
-            #  - truncated: The episode duration reaches max number of timesteps
-            #  - terminated: Any of the state space values is no longer finite.
             done = terminated or truncated
 
         reward_list.append(wrapped_env.return_queue[-1])
@@ -56,8 +46,6 @@ def train_agent(seed = 89, max_steps = 1000000):
         if episode % episode_batch == 0:
             avg_reward = int(np.mean(wrapped_env.return_queue))
             avg_episode_len = int(np.mean(wrapped_env.length_queue))
-            #print(f"return_queue = {wrapped_env.return_queue}")
-            #print(f"length_queue = {wrapped_env.length_queue}")
             rewards_to_plot.append(avg_reward)
             print("Episode:", episode, "Total Steps:", trainingSteps, "Average Reward:", avg_reward, "average episode len: ", avg_episode_len)
 
@@ -70,16 +58,15 @@ def train_agent(seed = 89, max_steps = 1000000):
 
 def replay_agent():
     env = gym.make('Hopper-v5', render_mode  = "human")
-    #env._max_episode_steps = 100000
+    env._max_episode_steps = 100000
     episode_batch = 100
-    wrapped_env = gym.wrappers.RecordEpisodeStatistics(env, episode_batch)  # Records episode-reward
+    wrapped_env = gym.wrappers.RecordEpisodeStatistics(env, episode_batch) 
 
     total_num_episodes = 100
     obs_space_dims = env.observation_space.shape[0]
     action_space_dims = env.action_space.shape[0]
     rewards_over_seeds = []
 
-    # Reinitialize agent every seed
     agent = Agent(obs_space_dims, action_space_dims)
     agent.net.load_state_dict(torch.load("./models/hopper", weights_only=True))
     agent.net.eval()
@@ -93,16 +80,9 @@ def replay_agent():
         while not done:
             action = agent.sample_action(obs)
 
-            # Step return type - `tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]`
-            # These represent the next observation, the reward from the step,
-            # if the episode is terminated, if the episode is truncated and
-            # additional info from the step
             obs, reward, terminated, truncated, info = wrapped_env.step(action)
             agent.rewards.append(reward)
 
-            # End the episode when either truncated or terminated is true
-            #  - truncated: The episode duration reaches max number of timesteps
-            #  - terminated: Any of the state space values is no longer finite.
             done = terminated or truncated
 
         episode += 1
